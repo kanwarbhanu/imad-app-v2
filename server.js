70766705de51d7a398148dc5d7d4f7487932a267
kanwarbhanu <kanwarbhanu@gmail.com>
2017-02-18 13:03:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/server.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'server.js'));
+app.get('/ui/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 
